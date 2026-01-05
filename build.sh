36be0b550c2a54c6b047549fe39dfe6c31c6beee
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-01-06 01:18:55 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ export SDK_INC="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/insta
 export SDK_LIB="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib"
 
 wget https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
-tar zxvf git-2.52.0.tar.gz 
+tar zxvf git-2.52.0.tar.gz > /dev/null
 cd git-2.52.0
 make CC=riscv64-unknown-linux-musl-gcc AR=riscv64-unknown-linux-musl-ar CFLAGS="-I$SDK_INC" LDFLAGS="-L$SDK_LIB" PREFIX=/usr/local/git NO_TCLTK=YesPlease NO_GETTEXT=YesPlease NO_CURL=YesPlease NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_ICONV=YesPlease NO_REGEX=NeedsStartEnd BLK_SHA1=YesPlease -j$(nproc)
 file git
