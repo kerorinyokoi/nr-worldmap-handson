b6a930335377003b67058a18d8be031dd036108a
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-01-06 01:42:31 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
-sudo apt update > /dev/null
-sudo apt install build-essential > /dev/null
-
-mkdir recamera && cd recamera
-
+mkdir recamera
+cd recamera
 wget -q https://github.com/Seeed-Studio/reCamera-OS/releases/download/0.2.1/sg2002_reCamera_0.2.1_emmc_sdk.tar.gz
 tar -xzvf sg2002_reCamera_0.2.1_emmc_sdk.tar.gz > /dev/null
-
-#Note: This tool is over 10GB in size. Please be aware of your disk space.
 git clone -q https://github.com/sophgo/host-tools.git --depth=1
 git clone -q https://github.com/Seeed-Studio/sscma-example-sg200x.git
-
 export SG200X_SDK_PATH=$PWD/sg2002_recamera_emmc
 export PATH=$PWD/host-tools/gcc/riscv64-linux-musl-x86_64/bin:$PATH
 export SDK_INC=$PWD/sg2002_recamera_emmc/install/soc_sg2002_recamera_emmc/tpu_musl_riscv64/cvitek_tpu_sdk/include
 export SDK_LIB=$PWD/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib
-
 riscv64-unknown-linux-musl-gcc -v
 
 wget -q https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
