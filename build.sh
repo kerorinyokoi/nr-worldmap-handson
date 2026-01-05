d25c0721751d7d2bcd5164c5409ab0d6c2a2242d
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-01-06 00:59:52 +0900

    Update and rename install.sh to build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,13 @@ git clone https://github.com/Seeed-Studio/sscma-example-sg200x.git
 export SG200X_SDK_PATH=$PWD/sg2002_recamera_emmc
 export PATH=$PWD/host-tools/gcc/riscv64-linux-musl-x86_64/bin:$PATH
 
+riscv64-unknown-linux-musl-gcc -v
+
 export SDK_INC="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/install/soc_sg2002_recamera_emmc/tpu_musl_riscv64/cvitek_tpu_sdk/include"
 export SDK_LIB="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib"
 
-riscv64-unknown-linux-musl-gcc -v
+wget https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
+tar zxvf git-2.52.0.tar.gz 
+cd git-2.52.0
+make CC=riscv64-unknown-linux-musl-gcc AR=riscv64-unknown-linux-musl-ar CFLAGS="-I$SDK_INC" LDFLAGS="-L$SDK_LIB" PREFIX=/usr/local/git NO_TCLTK=YesPlease NO_GETTEXT=YesPlease NO_CURL=YesPlease NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_ICONV=YesPlease NO_REGEX=NeedsStartEnd BLK_SHA1=YesPlease -j$(nproc)
+file git
