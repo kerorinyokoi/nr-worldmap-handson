b2a630cbacc95ead8432f7e2d7063fb178a696ad
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-01-06 01:36:20 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,11 +12,10 @@ git clone -q https://github.com/Seeed-Studio/sscma-example-sg200x.git
 
 export SG200X_SDK_PATH=$PWD/sg2002_recamera_emmc
 export PATH=$PWD/host-tools/gcc/riscv64-linux-musl-x86_64/bin:$PATH
+export SDK_INC=$PWD/sg2002_recamera_emmc/install/soc_sg2002_recamera_emmc/tpu_musl_riscv64/cvitek_tpu_sdk/include
+export SDK_LIB=$PWD/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib
 
 riscv64-unknown-linux-musl-gcc -v
-
-export SDK_INC="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/install/soc_sg2002_recamera_emmc/tpu_musl_riscv64/cvitek_tpu_sdk/include"
-export SDK_LIB="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib"
 
 wget -q https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
 tar zxvf git-2.52.0.tar.gz > /dev/null
