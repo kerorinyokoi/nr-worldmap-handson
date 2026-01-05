1059f88951b4e075f86fb280892650ad83528f8f
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-01-06 01:29:04 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ wget -q https://github.com/Seeed-Studio/reCamera-OS/releases/download/0.2.1/sg20
 tar -xzvf sg2002_reCamera_0.2.1_emmc_sdk.tar.gz > /dev/null
 
 #Note: This tool is over 10GB in size. Please be aware of your disk space.
-git clone https://github.com/sophgo/host-tools.git --depth=1
-git clone https://github.com/Seeed-Studio/sscma-example-sg200x.git
+git clone -q https://github.com/sophgo/host-tools.git --depth=1
+git clone -1 https://github.com/Seeed-Studio/sscma-example-sg200x.git
 
 export SG200X_SDK_PATH=$PWD/sg2002_recamera_emmc
 export PATH=$PWD/host-tools/gcc/riscv64-linux-musl-x86_64/bin:$PATH
@@ -18,7 +18,7 @@ riscv64-unknown-linux-musl-gcc -v
 export SDK_INC="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/install/soc_sg2002_recamera_emmc/tpu_musl_riscv64/cvitek_tpu_sdk/include"
 export SDK_LIB="/workspaces/codespaces-blank/recamera/sg2002_recamera_emmc/buildroot-2021.05/output/cvitek_CV181X_musl_riscv64/host/riscv64-buildroot-linux-musl/sysroot/usr/lib"
 
-wget https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
+wget -q https://www.kernel.org/pub/software/scm/git/git-2.52.0.tar.gz
 tar zxvf git-2.52.0.tar.gz > /dev/null
 cd git-2.52.0
 make CC=riscv64-unknown-linux-musl-gcc AR=riscv64-unknown-linux-musl-ar CFLAGS="-I$SDK_INC" LDFLAGS="-L$SDK_LIB" PREFIX=/usr/local/git NO_TCLTK=YesPlease NO_GETTEXT=YesPlease NO_CURL=YesPlease NO_EXPAT=YesPlease NO_OPENSSL=YesPlease NO_ICONV=YesPlease NO_REGEX=NeedsStartEnd BLK_SHA1=YesPlease -j$(nproc)
